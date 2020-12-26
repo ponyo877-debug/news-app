99a4095107506aa29353ec006ccddc32e0df1011
ponyo877-debug <hidakakis@nttdata.co.jp>
2020-12-26 11:18:45 +0900

    num of machine 2->1

---
+++ flutter/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'screens/news_list_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "まとめくん",
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: NewsListScreen(),
+    );
+  }
+}
